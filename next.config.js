6158ad24666408f6951906f6fc81d6612b90eead
!Taka <kontol543215@gmail.com>
2026-02-15 20:00:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'console.aichixia.xyz',
-          },
-        ],
-        destination: '/console',
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'console.aichixia.xyz',
-          },
-        ],
-        destination: '/console/:path*',
-      },
-    ]
-  },
+  swcMinify: true
 };
+
+module.exports = nextConfig;
