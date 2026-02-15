4d6755ca47c95626e3e5b22fd7f685d4519e452d
Reinaaa <aryaganz1212@gmail.com>
2026-02-16 05:19:20 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,50 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          destination: '/console/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'console.aichixia.xyz',
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  async redirects() {
     return [
       {
-        source: '/:path*',
-        destination: '/console/:path*',
+        source: '/pages/console/:path*',
+        destination: '/404',
+        permanent: false,
         has: [
           {
             type: 'host',
-            value: 'console.aichixia.xyz',
+            value: 'aichixia.xyz',
+          },
+        ],
+      },
+      {
+        source: '/console/:path*',
+        destination: '/404',
+        permanent: false,
+        has: [
+          {
+            type: 'host',
+            value: 'aichixia.xyz',
           },
         ],
       },
     ];
   },
-}
+};
+
+module.exports = nextConfig;
